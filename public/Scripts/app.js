21c6786a8baa9107a862e27fcc3d75a3de5e44d7
bysnakejnr <barisberber.ca@gmail.com>
2022-02-05 20:37:00 -0500

    Portfolio

---
+++ public/Scripts/app.js
@@ -0,0 +1,10 @@
+//IIFE - Immediately Invoked Function expression
+
+(function(){
+    function Start(){
+
+        console.log("Our express App Started......");
+    }
+    window.addEventListener("load",Start);
+
+})();
